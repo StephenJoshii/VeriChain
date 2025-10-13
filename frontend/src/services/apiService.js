7ee0cdf976bf344455129c54c8f872d4b788e1da
stephenjoshi <stephenjoshi17@gmail.com>
2025-10-13 15:29:41 -0500

    pinata function added

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const AI_API_URL = "http://127.0.0.1:5001";
 
 export const validateFace = async (imageData) => {
@@ -16,4 +18,20 @@ export const registerFace = async (name, images) => {
     body: JSON.stringify({ name, images }),
   });
   return response.json();
+};
+
+
+export const uploadToPinata = async (imageFile) => {
+  const formData = new FormData();
+  formData.append('file', imageFile);
+
+  const response = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+    headers: {
+      'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+      'Authorization': `Bearer ${import.meta.env.VITE_PINATA_JWT}`
+    }
+  });
+
+
+  return response.data.IpfsHash;
 };
